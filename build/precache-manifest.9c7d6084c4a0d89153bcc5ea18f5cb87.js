10024efe8d6ce783636c24dd5040d5ef256cd47f
Julien Capellari <julien@capellari.net>
2019-08-20 00:09:05 +0200

    version 1.1
    add: explorerf
    add: sight function
    update: floor management

---
+++ build/precache-manifest.9c7d6084c4a0d89153bcc5ea18f5cb87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab9407d2a5b3e8b8165a00b8455b49f5",
+    "url": "/rover/index.html"
+  },
+  {
+    "revision": "3a854ee0dfbcd3c9f762",
+    "url": "/rover/static/css/main.82ff97f0.chunk.css"
+  },
+  {
+    "revision": "64fb292cd31247ce52cc",
+    "url": "/rover/static/js/2.ad5e0817.chunk.js"
+  },
+  {
+    "revision": "3a854ee0dfbcd3c9f762",
+    "url": "/rover/static/js/main.aa123151.chunk.js"
+  },
+  {
+    "revision": "ad22e9aed79e7a4aef0f",
+    "url": "/rover/static/js/runtime~main.de383b40.js"
+  }
+]);
